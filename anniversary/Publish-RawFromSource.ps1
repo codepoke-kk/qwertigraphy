815793ac6ea2fc20311f8b5f1e27f5acebba2f65
Kevin Knox <kevinknox@embarqmail.com>
2021-01-11 21:51:15 -0500

    Corrected error in raw source

--- anniversary/Publish-RawFromSource.ps1
+++ anniversary/Publish-RawFromSource.ps1
@@ -18,12 +18,13 @@ foreach ($sourceLine in $sourceDictionaryLines) {
     if (-not $started) {Continue}
     if (-not $sourceline.length) {Continue}
 
-    # Transformations
+    # Transformations to just plain correct errors in the original source 
     if ($sourceline -match '^a-b-u  ') {Continue}
     if ($sourceline -match 'this') {$sourceline = $sourceline -replace 'th-s', 'ths'}
     $sourceline = $sourceline -replace 'th20', 'th2)'
     $sourceline = $sourceline -replace 'n-j-e k-s/k-sh', 'n-j-e/k-s/k-sh'
     $sourceline = $sourceline -replace 'ths \(s1\)', 'ths     '
+    $sourceline = $sourceline -replace 'condititon', 'condition'
 
     # $sourceline = $sourceline -replace 's-([oufvnmkgtd])', 's2-$1'
     $outlines.Add($sourceline) | Out-Null
