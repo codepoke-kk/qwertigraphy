a33cb7c22eaa73a3cd572d3ad319a26181e36b24
Kevin Knox <kevinknox@embarqmail.com>
2021-06-12 11:41:58 -0400

    Don't build trainer any more.

--- anniversary/New-Release.ps1
+++ anniversary/New-Release.ps1
@@ -1,5 +1,3 @@
 ﻿cd "$PsScriptRoot\..\qwertigraph"
 
-& "C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in trainer.ahk /out ..\release\trainer.exe /icon coach.ico
-
 & "C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in chorder.ahk /out ..\release\chorder.exe /icon coach.ico
