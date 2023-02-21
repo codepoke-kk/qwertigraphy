f112f1fd8f7185d6348357c0a3379c613f60dfce
Kevin Knox <kevinknox@embarqmail.com>
2023-02-20 20:57:49 -0500

    Quick counter

---
+++ utilities/OdometerCounts.ps1
@@ -0,0 +1,21 @@
+﻿$odometer1 = 'C:\Users\kevin\AppData\Roaming\Qwertigraph\odometerLifetime_20220115.ssv'
+$odometer2 = 'C:\Users\kevin\AppData\Roaming\Qwertigraph\odometerLifetime.ssv'
+
+$wordlines = Get-Content -Path $odometer1
+$wordlines += Get-Content -Path $odometer2
+
+$wins = 0
+foreach ($wordline in $wordlines) {
+    ($savings,$word,$qwerd,$chord,$form,$power,$saves,$matches,$chords,$misses,$other) = $wordline -split ';'
+    try {
+        if ($savings -notmatch '-') {
+            $wins = $wins + ($savings) 
+        }
+    } catch {
+        #Write-Error "Oops on $wordline giving $saves and $matches"
+    }
+}
+"$wins characters"
+"$($wins / 5) words"
+"$($wins / (5 * 30)) minutes"
+"$($wins / (5 * 30 * 60)) hours"
